7ef2ea28144db6e6e178bc63346c2f6f9d8c3cae
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-04 14:01:31 +1100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CardActionTypes from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CardActionTypes.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = (item) => ({
+  type: CardActionTypes.ADD_ITEM,
+  payload: item,
+});
